8e8a813adff9bc91e6e162c507a4e0fa90097ee5
Renaud Bédard <renaud.bedard@gmail.com>
2020-08-07 07:25:38 -0400

    ?????

--- src/commands/simon.js
+++ src/commands/simon.js
@@ -3390,17 +3390,19 @@ const simonDb = [
 ];
 
 function randn_bm(prng, min, max, skew) {
-  let u = 0, v = 0;
+  let u = 0,
+    v = 0;
   while(u === 0) u = prng(); //Converting [0,1) to (0,1)
   while(v === 0) v = prng();
-  let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
 
   num = num / 10.0 + 0.5; // Translate to 0 -> 1
   if (num > 1 || num < 0) num = randn_bm(prngm, min, max, skew); // resample between 0 and 1 if out of range
   num = Math.pow(num, skew); // Skew
   num *= max - min; // Stretch to fill range
   num += min; // offset to min
-  return num
+  return num;
+}
 
 function formatReviewSlackMessage(source, query, beerInfo) {
   // See https://api.slack.com/docs/message-formatting
