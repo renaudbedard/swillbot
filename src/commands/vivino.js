6122d283b7ffd08e8c7b72f3938645469fcc0ef7
Renaud Bédard <renaud.bedard@gmail.com>
2022-03-01 22:08:31 -0500

    50 really is the threshold huh

--- src/commands/vivino.js
+++ src/commands/vivino.js
@@ -12,11 +12,11 @@ const http = require("http");
 const https = require("https");
 const moment = require("moment");
 
-const agent = new http.Agent({ keepAlive: false });
-const secureAgent = new https.Agent({ keepAlive: false });
+const agent = new http.Agent({ keepAlive: true });
+const secureAgent = new https.Agent({ keepAlive: true });
 
-const waitFor = 5;
-const requestsPerBatch = 10;
+const waitFor = 120;
+const requestsPerBatch = 50;
 
 var requestsLeft = requestsPerBatch;
 var refillPending = false;
