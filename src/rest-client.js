e6ca8edd5555df750071c050d424cc80ea9a62ae
Renaud Bédard <renaud.bedard@gmail.com>
2023-03-12 16:41:10 -0400

    why won't you wooooooork though

--- src/rest-client.js
+++ src/rest-client.js
@@ -9,10 +9,10 @@ client.lastGetTimestamp = Date.now();
 
 client.rateLimitGet = async (url, args, callback) => 
 {
-    while (Date.now() - client.lastGetTimestamp < 250)
+    while (Date.now() - client.lastGetTimestamp < 1000)
     {
-        console.log(`Rate limiting for ${url}...`);
-        await new Promise(r => setTimeout(r, 250));
+        //console.log(`Rate limiting for ${url}...`);
+        await new Promise(r => setTimeout(r, 1000));
     }
     client.lastGetTimestamp = Date.now();
     return client.get(url, args, callback);
