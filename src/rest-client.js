073af60a13dd73666bb98527f6b997c2348b0de3
Renaud Bédard <renaud.bedard@gmail.com>
2023-03-12 19:14:50 -0400

    ah yes, the ol' includes vs contains debate

--- src/rest-client.js
+++ src/rest-client.js
@@ -5,12 +5,10 @@
 const Client = require("node-rest-client-promise").Client;
 const client = new Client();
 
-client.lastGetTimestamp = Date.now();
-
 client.rateLimitGet = (url, args, callback) => 
 {
     return client.get(url, args, function(data, _) {
-        if (data.toString("utf8").contains("error code: 1015")) {
+        if (data.toString("utf8").includes("error code: 1015")) {
             console.log("Got rate limited, will retry in 5 seconds...");
             setTimeout(() => {
                 client.rateLimitGet(url, args, callback);
